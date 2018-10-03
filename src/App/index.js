4988381c5dbe58cbbe035a35afd57b66aae471a7
Vladimir Guguiev <wizardzloy@gmail.com>
2018-10-03 08:52:28 +0200

    fetch data on the Home page with react-apollo

--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,26 @@
 // @flow
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from "react-apollo";
 
 import Home from '../Home';
 import Detail from '../Detail';
 import AppLayout from './AppLayout';
 
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:8080',
+});
 
 const App = () => (
-  <Router>
-    <AppLayout>
-      <Route exact path="/" component={Home} />
-      <Route path="/film/:id" component={Detail} />
-    </AppLayout>
-  </Router>
+  <ApolloProvider client={apolloClient}>
+    <Router>
+      <AppLayout>
+        <Route exact path="/" component={Home} />
+        <Route path="/film/:id" component={Detail} />
+      </AppLayout>
+    </Router>
+  </ApolloProvider>
 );
 
 export default App;
--- src/Home/index.js
+++ src/Home/index.js
@@ -1,13 +1,70 @@
 // @flow
 import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 import { Link } from 'react-router-dom';
+import FilmsList from './FilmsList';
+import FilmsListItem from './FilmsListItem';
+import FilmPoster from './FilmPoster';
 
+const query = gql`
+  query FilmsData {
+    allFilms {
+      films {
+        id
+        title
+        episodeID
+        poster
+      }
+    }
+  }
+`;
+
+type FilmsDataType = {
+  allFilms: ?{
+    films: $ReadOnlyArray<?{
+      id: string,
+      title: ?string,
+      episodeID: ?number,
+      poster: ?string,
+    }>
+  }
+};
+
+class FilmsQuery extends Query<FilmsDataType, {}> {}
 
 const Home = () => (
-  <div>
-    <div>Home</div>
-    <Link to="/film/fake-id">Link to Detail Page</Link>
-  </div>
+  <FilmsQuery query={query}>
+    {({ loading, error, data }) => {
+      if (loading) return <p>Home page is loading...</p>;
+      if (error) return <p>Error happened! {error.message}</p>;
+      if (!data || !data.allFilms) return <p>Unexpected query result</p>;
+
+      const films = data.allFilms.films.filter(Boolean);
+
+      return (
+        <FilmsList>
+          {films.map(({ id, title, episodeID, poster }) => {
+            const posterTitle = (episodeID != null) ? `Episode ${episodeID}` : 'Unknown episode';
+            const posterSubtitle = title || '';
+            const posterImageUrl = poster && `http://localhost:8080${poster}`;
+
+            return (
+              <FilmsListItem key={id}>
+                <Link to={`/film/${id}`} style={{ textDecoration: 'none' }}>
+                  <FilmPoster
+                    title={posterTitle}
+                    subtitle={posterSubtitle}
+                    imageUrl={posterImageUrl}
+                  />
+                </Link>
+              </FilmsListItem>
+            );
+          })}
+        </FilmsList>
+      )
+    }}
+  </FilmsQuery>
 );
 
 
